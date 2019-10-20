01ddba1cd34cb1dd5386c1c75462d3e800071664
Mauricio CV <mauricio@rayolaser.pe>
2019-10-20 17:04:25 -0500

    First commit

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -31,8 +31,8 @@ export default class Instagram extends React.Component {
                         <div className="insta-wrapper" style={{ backgroundColor: '#111111', width: '100%', flexWrap: 'wrap', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
                                 {this.state.images.map((image) => { return(
                                     <div className='post-container' style={{ position: 'relative', minWidth: '120px', width: '23.5%', marginTop: 0, marginLeft: 0, marginRight: 0, marginBottom: 2}}>
-                                        <img alt={image.images.id} style={{ flex: 1, height: '90%', width: '90%'}} src={image.images.standard_resolution.url}>
-                                        </img>
+                                        <a href={image.link}>
+                                            <img className='ig-image' alt={image.images.id} style={{ flex: 1, height: '90%', width: '90%'}} src={image.images.standard_resolution.url} /></a>
                                     </div>
                                 ) }
                                 )}
