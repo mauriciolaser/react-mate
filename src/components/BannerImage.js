36d5978e3cf121f102ad80a073f73b7c0b1f5937
Mauricio CV <mauricio@rayolaser.pe>
2019-10-20 13:48:40 -0500

    First commit

--- src/components/BannerImage.js
+++ src/components/BannerImage.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function BannerImage() {
     return (
-        <div className='App-bannerimage' style={{minHeight: 514, minWidth: 514}}>
-            <img style={{ height: '100%', width: '100%' }} src={'https://format-com-cld-res.cloudinary.com/image/private/s--NvcsSFEV--/c_crop,h_514,w_514,x_0,y_0/c_fill,g_center,h_987,w_2000/a_auto,dpr_2,fl_keep_iptc.progressive,q_95/v1/d6583862766db87e8d431313f3938778/BODEGA_MATE6_1_.jpg'} alt='nepe'/>
+        <div className='App-bannerimage' style={{width: '100%', height: '100%'}}>
+            <img style={{ height: '100%', width: '100%' }} src={process.env.PUBLIC_URL + '/assets/banner.jpg'} alt='nepe'/>
         </div>
     )
 }
