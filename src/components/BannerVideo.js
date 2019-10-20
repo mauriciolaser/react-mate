4263a9a34eb138343715b5925af958e776a10850
Mauricio CV <mauricio@rayolaser.pe>
2019-10-20 14:14:23 -0500

    First commit

--- src/components/BannerVideo.js
+++ src/components/BannerVideo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BannerVideo() {
     return (
         <div style={{ backgroundColor: '#111111'}}>
-                <video id="background-video" loop autoPlay style={{width: '100%', }}>
+                <video id="background-video" loop autoPlay muted style={{width: '100%', }}>
                 <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
