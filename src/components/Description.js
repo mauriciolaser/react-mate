7d992ad051f77e8d8b3335cf4e94f0f4d0c1c060
Mauricio CV <mauricio@rayolaser.pe>
2019-10-20 14:12:47 -0500

    First commit

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,19 +3,21 @@ import React from 'react'
 
 export default function Description() {
 
-    const MainDescription = 'Bodega MATE es una plataforma de arte, moda\ny diseño con la visión de poner en valor la \nherencia latinoamericana.';
-    const SecondaryDescription = 'Con casi 60 marcas y 11 países de la región, Bodega MATE tiene como\nobjetivo ayudar a que el museo MATE alcance la autosostenibilidad\ncontinue con su misión educativa y cultural como espacio de exhibiciones y como catalizador de la industria creativa local.';
+    const MainDescription = 'Bodega MATE es una plataforma de arte, moda y diseño con la visión de poner en valor la herencia latinoamericana.';
+    const SecondaryDescription = 'Con casi 60 marcas y 11 países de la región, Bodega MATE tiene como objetivo ayudar a que el museo MATE alcance la autosostenibilidad continue con su misión educativa y cultural como espacio de exhibiciones y como catalizador de la industria creativa local.';
 
     return (
         <div style={{ width: '100%', backgroundColor: '#111111', display: 'flex', justifyContent: 'center'}}>
-            <div className='App-description' style={{ display: 'flex', flexDirection: 'column', color: 'pink', maxWidth: 1100, backgroundColor: '#111111', height: '100%', marginTop: 0, alignItems: 'stretch'}}>
+            <div className='App-description' style={{ display: 'flex', flexDirection: 'column', color: 'pink', maxWidth: 1100, backgroundColor: '#111111', height: '100%', marginTop: 0, alignItems: 'center'}}>
     
-                <h2 style={{ flex: 1, padding: 10, whiteSpace: 'pre-line', fontWeight: 'bold'}}>
-                        {MainDescription}
-                </h2>
-                <h1 style={{ flex: 1, padding: 10, fontSize: 22, whiteSpace: 'pre-line', fontWeight: 'bold' }}>
-                    {SecondaryDescription}
-                </h1>
+                <div style={{width: '75%'}}>
+                    <h3 style={{ flex: 1, padding: 10, whiteSpace: 'pre-line', fontWeight: 'bold'}}>
+                            {MainDescription}
+                    </h3>
+                    <h5 style={{ flex: 1, padding: 10, whiteSpace: 'pre-line', fontWeight: 'bold' }}>
+                        {SecondaryDescription}
+                    </h5>
+                </div>
     
             </div>
         </div>
