089b48860885becd71fb6be9fb399009da229359
Mauricio CV <mauricio@rayolaser.pe>
2019-10-23 14:50:30 -0500

    First commit

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -71,9 +71,9 @@ const CustomForm = () => (
 const MediaLinks = () => {
     return (
         <div style={{direction: 'flex', flexDirection: 'row', color: 'white'}}>
-            <InstagramIcon className='icon-newsletter'/>
+            <a href="https://www.instagram.com/bodega_mate/?hl=es"><InstagramIcon className='icon-newsletter'/></a>
                 
-            <FacebookIcon className='icon-newsletter'/>
+            <a href="https://www.facebook.com/pg/MuseoMATE/photos/?tab=album&album_id=330615733701705"><FacebookIcon className='icon-newsletter'/></a>
         </div>
     )
 }
